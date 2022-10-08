35b02865ef1ebdc6bae2b3e461af289e5ede3bba
happyGGang <joy.gang@dxworks.kr>
2022-10-08 17:44:58 +0900

    [update] postList api 세팅

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -2,9 +2,36 @@
 	<div>
 		<h1>게시글 목록</h1>
 		<hr class="my-4" />
+		<div class="row g-3">
+			<div v-for="post in posts" :key="post.id" class="col-4">
+				<PostItem
+					:title="post.title"
+					:content="post.content"
+					:created-at="post.createdAt"
+					@click="goPage(post.id)"
+				></PostItem>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import PostItem from '@/components/posts/PostItem.vue';
+import { getPosts } from '@/api/posts.js';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const posts = ref([]);
+
+const fetchPosts = () => {
+	posts.value = getPosts();
+};
+fetchPosts();
+
+const goPage = id => {
+	router.push(`/posts/${id}`);
+};
+</script>
 
 <style lang="scss" scoped></style>
