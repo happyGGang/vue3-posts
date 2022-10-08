53059ccb843b4d86687a1cec1005fd2b90dff369
happyGGang <joy.gang@dxworks.kr>
2022-10-08 23:26:45 +0900

    [update] 상세페이지 마크업

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,7 +1,27 @@
 <template>
 	<div>
-		<h1>게시글 상세보기</h1>
+		<h1>제목</h1>
+		<p>내용</p>
+		<p>202-01-01</p>
 		<hr class="my-4" />
+		<div class="row g-2">
+			<div class="col-auto">
+				<button class="btn btn-outline-dark">이전</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-dark">다음</button>
+			</div>
+			<div class="col-auto me-auto"></div>
+			<div class="col-auto">
+				<button class="btn btn-outline-dark">목록</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-primary">수정</button>
+			</div>
+			<div class="col-auto">
+				<button class="btn btn-outline-danger">삭제</button>
+			</div>
+		</div>
 	</div>
 </template>
 
