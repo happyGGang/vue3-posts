0b4763cf84ee8810a751df4bf58c94ab4a3a19a0
happyGGang <joy.gang@dxworks.kr>
2022-10-08 13:31:25 +0900

    [update] 네비바 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,52 @@
 <script setup></script>
 
 <template>
-	<main>setting</main>
+	<header>
+		<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#"
+								>HappyGGang</a
+							>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">About</a>
+						</li>
+					</ul>
+					<form class="d-flex" role="search">
+						<input
+							class="form-control me-2"
+							type="search"
+							placeholder="Search"
+							aria-label="Search"
+						/>
+						<button class="btn btn-outline-success" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<div class="container py-4">
+			<h2>hello vue</h2>
+		</div>
+	</main>
 </template>
 
 <style></style>
