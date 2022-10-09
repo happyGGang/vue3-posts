8928272b5ebe5d62e09ab4a50aa43905cb1dbee6
happyGGang <joy.gang@dxworks.kr>
2022-10-09 23:28:13 +0900

    [update] 페이지네이션 구현

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -14,16 +14,36 @@
 		</div>
 		<nav class="mt-5" aria-label="Page navigation example">
 			<ul class="pagination justify-content-center">
-				<li class="page-item">
-					<a class="page-link" href="#" aria-label="Previous">
+				<li class="page-item" :class="{ disabled: !(params._page > 1) }">
+					<a
+						class="page-link"
+						href="#"
+						aria-label="Previous"
+						@click.prevent="--params._page"
+					>
 						<span aria-hidden="true">&laquo;</span>
 					</a>
 				</li>
-				<li class="page-item"><a class="page-link" href="#">1</a></li>
-				<li class="page-item"><a class="page-link" href="#">2</a></li>
-				<li class="page-item"><a class="page-link" href="#">3</a></li>
-				<li class="page-item">
-					<a class="page-link" href="#" aria-label="Next">
+				<li
+					v-for="page in pageCount"
+					:key="page"
+					class="page-item"
+					:class="{ active: params._page === page }"
+				>
+					<a class="page-link" href="#" @click.prevent="++params._page">{{
+						page
+					}}</a>
+				</li>
+				<li
+					class="page-item"
+					:class="{ disabled: !(params._page < pageCount) }"
+				>
+					<a
+						class="page-link"
+						href="#"
+						aria-label="Next"
+						@click.prevent="++params._page"
+					>
 						<span aria-hidden="true">&raquo;</span>
 					</a>
 				</li>
@@ -35,26 +55,32 @@
 <script setup>
 import PostItem from '@/components/posts/PostItem.vue';
 import { getPosts } from '@/api/posts.js';
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
+import { computed } from 'vue';
 
 const router = useRouter();
 const posts = ref([]);
 const params = ref({
 	_sort: 'createdAt',
 	_order: 'desc',
+	_limit: 3,
+	_page: 1,
 });
 
+const totalCount = ref(0);
+const pageCount = computed(() => totalCount.value / params.value._limit);
 const fetchPost = async () => {
 	try {
-		const { data } = await getPosts(params);
+		const { data, headers } = await getPosts(params.value);
+		totalCount.value = headers['x-total-count'];
 		posts.value = data;
 	} catch (error) {
 		console.log(error);
 	}
 };
-fetchPost();
-
+//fetchPost();
+watchEffect(fetchPost);
 const goPage = id => {
 	//router.push(`/posts/${id}`);
 	router.push({
