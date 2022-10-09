9804b4c9b1089f99bb2eefc856320d9e37899e87
happyGGang <joy.gang@dxworks.kr>
2022-10-09 18:35:46 +0900

    [update] 상세페이지 구현

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-		<h1>{{ form.title }}</h1>
-		<p>{{ form.content }}</p>
-		<p>{{ form.createdAt }}</p>
+		<h1>{{ post.title }}</h1>
+		<p>{{ post.content }}</p>
+		<p>{{ post.createdAt }}</p>
 		<hr class="my-4" />
 		<div class="row g-2">
 			<div class="col-auto">
@@ -35,11 +35,16 @@ import { ref } from 'vue';
 const route = useRoute();
 const router = useRouter();
 const id = route.params.id;
-const form = ref({});
+const post = ref({});
 
-const fetchPost = () => {
-	const data = getPostById(id);
-	form.value = { ...data };
+const fetchPost = async () => {
+	const { data } = await getPostById(id);
+	setPost(data);
+};
+const setPost = ({ title, content, createAt }) => {
+	post.value.title = title;
+	post.value.content = content;
+	post.value.createAt = createAt;
 };
 fetchPost();
 const goListPage = () => router.push({ name: 'PostListView' });
