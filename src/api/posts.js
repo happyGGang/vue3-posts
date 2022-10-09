52f065bc65ea5641be533baf6d7af6a11b3bbe4e
happyGGang <joy.gang@dxworks.kr>
2022-10-09 14:26:35 +0900

    [del] 미사용 import 삭제

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,3 @@
-import { isTemplateNode } from '@vue/compiler-core';
-
-import { findDir } from '@vue/compiler-core';
-
 const posts = [
 	{ id: 1, title: '제목1', content: '내용1', createdAt: '2020-01-01' },
 	{ id: 2, title: '제목2', content: '내용2', createdAt: '2020-01-02' },
